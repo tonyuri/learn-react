29c3e73124d8f2da87ceab75a67c485d0934fba3
tonyuri <toeysos0@gmail.com>
2025-12-02 18:04:56 +0700

    Learn useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,42 @@
-import { act, Fragment, useState } from 'react'
+import { act, Fragment, use, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
-// Components
-import Greeting from './components/Greeting.jsx'
-import Card from './components/Card.jsx'
-import Layout from './components/Layout.jsx'
-import Userlist from './components/UserList.jsx'
-import Categories from './components/Categories.jsx'
 
 function App() {
-  const [bgColor, setBgColor] = useState('Lightgrey')
-  const position = "Front-end Developer"
-  const items = ["Apple", "Banan", "Orange"]
+  const [count, setCount] = useState(0);
+  const [user, setUser] = useState({name:"John", age:30});
+  const [todos, setTodos] = useState(["React", "JavaScript"])
 
-  const handleClick = (name) => {
-    alert(`Hello, ${name}`)
+  const updateAge = () => {
+    // Spread Operator (...)
+    setUser(prevState => ({...prevState, age: prevState.age +1 }))
   }
-
-  const handleChange = (event) => {
-    console.log("Input value:", event.target.value)
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault(); // Prevent page reload
-    alert("Form submitted")
-  }
-
-  const handleMouseEnter = () => setBgColor('Lightblue')
-  const handleMouseLeave = () => setBgColor('Lightgrey')
-
-  const handleKeyDown = (event) => console.log("Key pressed:", event.key)
   
-
+  
+  
   return (
-    <Layout>
-      {/* Child components */}
-      <Greeting name="John" age={30} position={position} isLoggedIn={true} hasMessage={true}/>
-      <Card title="ReactJS">
-        Learn ReactJS for beginners
-      </Card>
-      <Userlist />
-      <Categories />
-      <form onSubmit={handleSubmit}>
-        <input type='text' onChange={handleChange} />
-        <button type="submit">Click me</button>
-      </form>
-      <div 
-        onMouseEnter={handleMouseEnter} 
-        onMouseLeave={handleMouseLeave}
-        style={{padding: "20px", backgroundColor: bgColor, transition: "0.3s"}}
-      >
-        Hover over me! here
-      </div>
-      <input type="text" onKeyDown={handleKeyDown} onFocus={() => console.log('Input focused')} onBlur={() => console.log('Input blurred')}/>
-      <div style={{backgroundColor: "yellow", padding: "20px", borderRadius: "10px"}}>
-        Inline CSS
-      </div>
-    </Layout>
+   <div>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+      <button className="border p-3 m-3" onClick={updateAge}>Update age</button>
+
+      <p>Count: {count}</p>
+      <button className="border p-3 m-3" onClick={() => setCount(count+1)}>เพิ่ม</button>
+      <button className="border p-3 m-3" onClick={() => setCount(count-1)}>ลด</button>
+
+      <hr />
+      
+      <h3 className='font-bold'>Todo list</h3>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+
+   </div>
   )
 }
 
