02f948b24cf331cc6f06992a42d88f10d77de738
tonyuri <toeysos0@gmail.com>
2025-12-03 10:08:27 +0700

    Add comment at Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import styles from '../styles.module.css'
 import styled from 'styled-components'
 
+// Write CSS in JavaScript
 const Container = styled.div`
     max-width: 1320px;
     margin: 0 auto;
